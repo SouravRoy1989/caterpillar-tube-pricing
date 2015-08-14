0f2fd316ffb65603595cd2b95e8d70408fc55de2
marknagelberg <nagelbergmark@gmail.com>
2015-08-14 00:22:42 -0500

    First pass at including all of the relevant comp variables. Scores .295 on leaderboard

--- modelling.py
+++ modelling.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import Imputer
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 import os
+import csv
 
 def extract(data):
 	
@@ -71,6 +72,290 @@ def extract(data):
 			if row[var] in component_mapping:
 				data_merged.set_value(idx, component_mapping[row[var]], 1)
 
+
+	#Additional processing of component data and adding it to train/test.
+	comp_files = ['comp_adaptor.csv', 'comp_boss.csv', 'comp_elbow.csv', 'comp_float.csv', 'comp_hfl.csv', 'comp_nut.csv', 'comp_other.csv', 'comp_sleeve.csv', 'comp_straight.csv', 'comp_tee.csv', 'comp_threaded.csv']
+	component_names = ['component_id_' + str(x) for x in range(1, 9)]
+	concat_final_output = []
+
+	#Build a dictionary with component_id as key, and value a dictionary of variables an values it can take.
+	comp_variables = {}
+	for comp_filename in comp_files:
+		comp_file = pd.read_csv('competition_data/' + comp_filename)
+		with open('competition_data/' + comp_filename) as csvfile:
+			comp_reader = csv.DictReader(csvfile)
+			for row in comp_reader:
+				comp_variables[row['component_id']] = row
+
+	counter = {x:0 for x in range(10)}
+	#Add all of the relevant component data.
+	for idx, row in data_merged.iterrows():
+		count = 0
+
+		weight = 0.0
+		names = []
+		orientation_yes = 0
+		orientation_no = 0
+		diameter = -1
+		total_nominal_size = 0
+		thread_pitch = 0
+		adaptor_angle = 0
+		total_length = 0
+		head_diameter = 0
+		corresponding_shell = []
+		outside_shape = []
+		outside_shape_round = 0
+		outside_shape_hex = 0
+		outside_shape_na = 0
+		total_base_diameter = 0
+		total_thread_size = 0
+		thread_count = 0
+		connection_type_counter = {'B-001':0, 'B-002':0,'B-003':0,'B-004':0,'B-005':0,'B-006':0,'B-007':0,'B-008':0,'B-009':0,'B-010':0,'B-011':0,'B-012':0,'B-013':0, '9999':0}
+		connection_type_ids = ["connection_type_id_" + str(x) for x in range(1,5)]
+		connection_type_ids.append("connection_type_id")
+		nut_and_sleeve_length = 0
+		thickness = 0
+		mj_plug_class_code = 0
+		base_type_flatbottom = 0
+		base_type_shoulder = 0
+		base_type_saddle = 0
+		hex_size = 0
+		seat_angle = 0
+		thread_size = 0
+		type_boss = 0
+		type_stud = 0
+		end_form_ids = {'A-00' + str(x):0 for x in range(1,8)}
+		intended_nut_pitch = 0
+		bolt_pattern_wide = 0
+		plug_diameter = 0
+		hex_nut_size = 0
+		height_over_tube = 0
+		shoulder_diameter = 0
+		overall_length = 0
+		extension_length = 0
+		intended_nut_thread = 0
+		mj_class_code_exists = 0
+		unique_feature_yes = 0
+		unique_feature_no = 0
+		drop_length = 0
+		elbow_angle = 0
+		bolt_pattern_long = 0
+		thread_pitches = {'thread_pitch_' + str(x):0 for x in range(1,5)}
+		groove_no = 0
+		groove_yes = 0
+
+		#Get the component names you'll be working with for the row
+		for name in component_names:
+			if row[name] != '' and row[name] != '9999':
+				names.append(row[name])
+		if names:
+			for name in names:
+				if comp_variables[name]['weight'] != 'NA':
+					weight += float(comp_variables[name]['weight'])
+				if 'orientation' in comp_variables[name]:
+					if comp_variables[name]['orientation'] == 'Yes':
+						orientation_yes += 1
+					if comp_variables[name]['orientation'] == 'No':
+						orientation_no += 1
+				if 'diameter' in comp_variables[name]:
+					if comp_variables[name]['diameter'] != 'NA':
+						diameter = float(comp_variables[name]['diameter'])
+				if 'nominal_size_1' in comp_variables[name]:
+					#Get nominal sizes (there are 4 in one file and 2 in another)
+					nominal_sizes = [x for x in comp_variables[name] if x[0:13] == 'nominal_size_']
+					total_nominal_size = sum(float(comp_variables[name][x]) for x in nominal_sizes if comp_variables[name][x] != 'NA' and comp_variables[name][x] != 'See Drawing')
+				#Note - a lot of records have a thread pitch - is it possible something can have multiple 
+				#components and thus multiple thread pitches? If so, want to think of proper way to aggregate.
+				if 'thread_pitch' in comp_variables[name]:
+					thread_pitch = comp_variables[name]['thread_pitch']
+				if 'adaptor_angle' in comp_variables[name]:
+					if comp_variables[name]['adaptor_angle'] != 'NA':
+						adaptor_angle = comp_variables[name]['adaptor_angle']
+				if 'length_1' in comp_variables[name]:
+					#Get lengths (there are 4 in one file and 2 in another)
+					lengths = [x for x in comp_variables[name] if x[0:7] == 'length_']
+					total_length = sum(float(comp_variables[name][x]) for x in lengths if comp_variables[name][x] != 'NA')
+				if 'head_diameter' in comp_variables[name]:
+					if comp_variables[name]['head_diameter'] != 'NA':
+						head_diameter = comp_variables[name]['head_diameter']
+				#There are no tubes with multiple components with corresponding shell
+				if 'corresponding_shell' in comp_variables[name]:
+					corresponding_shell.append(comp_variables[name]['corresponding_shell'])
+				#There are some tubes with multiple components with corresponding shell
+				if 'outside_shape' in comp_variables[name]:
+					if comp_variables[name]['outside_shape'] == "Round":
+						outside_shape_round += 1
+					if comp_variables[name]['outside_shape'] == "NA":
+						outside_shape_na += 1
+					if comp_variables[name]['outside_shape'] == "Hex":
+						outside_shape_hex += 1
+				if 'base_diameter' in comp_variables[name]:
+					if comp_variables[name]['base_diameter'] != 'NA':
+						total_base_diameter += float(comp_variables[name]['base_diameter'])
+				if 'thread_size' in comp_variables[name]:
+					thread_count += 1
+					if comp_variables[name]['thread_size'][0] == 'M':
+						#http://www.engineeringtoolbox.com/metric-threads-d_777.html
+						total_thread_size += float(comp_variables[name]['thread_size'][1:]) * 0.03937
+					else:
+						total_thread_size += float(comp_variables[name]['thread_size'])
+				if 'connection_type_id_1' in comp_variables[name] or 'connection_type_id' in comp_variables[name]:
+					for connection_type in connection_type_ids:
+						if connection_type in comp_variables[name] and comp_variables[name][connection_type] != 'NA':
+							connection_type_counter[comp_variables[name][connection_type]] += 1
+				if 'length' in comp_variables[name]:
+					if comp_variables[name]['length'] != '9999':
+						nut_and_sleeve_length += float(comp_variables[name]['length'])
+
+				if 'thickness' in comp_variables[name]:
+					if comp_variables[name]['thickness'] != 'NA':
+						thickness += float(comp_variables[name]['thickness'])
+				if 'mj_plug_class_code' in comp_variables[name] and comp_variables[name]['mj_plug_class_code'] != 'NA':
+					mj_plug_class_code += 1
+				if 'base_type' in comp_variables[name]:
+					if comp_variables[name]['base_type'] == "Shoulder":
+						base_type_shoulder += 1
+					if comp_variables[name]['base_type'] == "Flat Bottom":
+						base_type_flatbottom += 1
+					if comp_variables[name]['base_type'] == "Saddle":
+						base_type_saddle += 1
+				if 'hex_size' in comp_variables[name] and comp_variables[name]['hex_size'] != 'NA':
+					hex_size += float(comp_variables[name]['hex_size'])
+				if 'seat_angle' in comp_variables[name] and comp_variables[name]['seat_angle'] != 'NA':
+					seat_angle += float(comp_variables[name]['seat_angle'])
+				if 'thread_size_1' in comp_variables[name]:
+					thread_size_varnames = ['thread_size_1', 'thread_size_2', 'thread_size_3', 'thread_size_4']
+					for size in thread_size_varnames:
+						if size in comp_variables[name] and comp_variables[name][size] != 'NA':
+							thread_size += float(comp_variables[name][size])
+				if 'type' in comp_variables[name]:
+					if comp_variables[name]['type'] == 'Boss':
+						type_boss += 1
+					elif comp_variables[name]['type'] == 'Stud':
+						type_stud += 1
+				if 'end_form_id_1' in comp_variables[name]:
+					end_form_id_names = ['end_form_id_1', 'end_form_id_2', 'end_form_id_3', 'end_form_id_4']
+					for id_name in end_form_id_names:
+						if id_name in comp_variables[name] and comp_variables[name][id_name] != 'NA' and comp_variables[name][id_name] != "9999":
+							end_form_ids[comp_variables[name][id_name]] += 1
+				#Skipped "material" variable.
+				if 'intended_nut_pitch' in comp_variables[name]:
+					intended_nut_pitch += float(comp_variables[name]['intended_nut_pitch'])
+				if 'bolt_pattern_wide' in comp_variables[name] and comp_variables[name]['bolt_pattern_wide'] != "NA":
+					bolt_pattern_wide += float(comp_variables[name]['bolt_pattern_wide'])
+				if 'plug_diameter' in comp_variables[name] and comp_variables[name]['plug_diameter'] != "NA":
+					plug_diameter += float(comp_variables[name]['plug_diameter'])
+				if 'hex_nut_size' in comp_variables[name] and comp_variables[name]['hex_nut_size'] != "NA":
+					hex_nut_size += float(comp_variables[name]['hex_nut_size'])
+				#Skipped "blind_hole" variable
+				#Skipped "hose_diameter" variable
+				if 'height_over_tube' in comp_variables[name] and comp_variables[name]['height_over_tube'] != "NA":
+					height_over_tube += float(comp_variables[name]['height_over_tube'])
+				if 'shoulder_diameter' in comp_variables[name] and comp_variables[name]['shoulder_diameter'] != "NA":
+					shoulder_diameter += float(comp_variables[name]['shoulder_diameter'])
+				if 'overall_length' in comp_variables[name] and comp_variables[name]['overall_length'] != "NA":
+					overall_length += float(comp_variables[name]['overall_length'])
+				#Skipped "part_name" variable
+				if 'extension_length' in comp_variables[name] and comp_variables[name]['extension_length'] != "NA":
+					extension_length += float(comp_variables[name]['extension_length'])
+				if 'intended_nut_thread' in comp_variables[name] and comp_variables[name]['intended_nut_thread'] != "NA":
+					intended_nut_thread += float(comp_variables[name]['intended_nut_thread'])
+				if 'mj_class_code' in comp_variables[name] and comp_variables[name]['mj_class_code'] != "NA":
+					mj_class_code_exists = 1
+
+				if 'unique_feature' in comp_variables[name] and comp_variables[name]['unique_feature'] != "NA":
+					if comp_variables[name]['unique_feature'] == "Yes":
+						unique_feature_yes += 1
+					if comp_variables[name]['unique_feature'] == "No":
+						unique_feature_no += 1
+				if 'drop_length' in comp_variables[name] and comp_variables[name]['drop_length'] != "NA":
+					drop_length += float(comp_variables[name]['drop_length'])
+				if 'elbow_angle' in comp_variables[name] and comp_variables[name]['elbow_angle'] != "NA":
+					elbow_angle += float(comp_variables[name]['elbow_angle'])
+				#Skipped "coupling class" variable
+				if 'bolt_pattern_long' in comp_variables[name] and comp_variables[name]['bolt_pattern_long'] != "NA":
+					bolt_pattern_long += float(comp_variables[name]['bolt_pattern_long'])
+				if 'thread_pitch_1' in comp_variables[name]:
+					thread_pitch_names = ['thread_pitch_1', 'thread_pitch_2', 'thread_pitch_3', 'thread_pitch_4']
+					for p_name in thread_pitch_names:
+						if p_name in comp_variables[name] and comp_variables[name][p_name] != 'NA' and comp_variables[name][p_name] != "9999":
+							thread_pitches[p_name] += float(comp_variables[name][p_name])
+				if 'groove' in comp_variables[name] and comp_variables[name]['groove'] != "NA":
+					if comp_variables[name]['groove'] == "Yes":
+						groove_yes += 1
+					if comp_variables[name]['groove'] == "No":
+						groove_no += 1
+
+		data_merged.set_value(idx, 'total_weight', weight)
+		data_merged.set_value(idx, 'orentation_yes', orientation_yes)
+		data_merged.set_value(idx, 'orientation_no', orientation_no)
+		data_merged.set_value(idx, 'diameter', diameter)
+		data_merged.set_value(idx, 'total_nominal_size', total_nominal_size)
+		data_merged.set_value(idx, 'thread_pitch', thread_pitch)
+		data_merged.set_value(idx, 'adaptor_angle', adaptor_angle)
+		data_merged.set_value(idx, 'total_length', total_length)
+		data_merged.set_value(idx, 'outside_shape_round', outside_shape_round)
+		data_merged.set_value(idx, 'outside_shape_na', outside_shape_na)
+		data_merged.set_value(idx, 'outside_shape_hex', outside_shape_hex)
+		data_merged.set_value(idx, 'total_base_diameter', total_base_diameter)
+
+		if thread_count == 0:
+			data_merged.set_value(idx, 'average_thread_size', total_thread_size)
+		else:
+			data_merged.set_value(idx, 'average_thread_size', total_thread_size/thread_count)
+
+		for connection_type in connection_type_counter:
+			data_merged.set_value(idx, connection_type, connection_type_counter[connection_type])
+		data_merged.set_value(idx, 'nut_and_sleeve_length', nut_and_sleeve_length)
+		data_merged.set_value(idx, 'thickness', thickness)
+		data_merged.set_value(idx, 'mj_plug_class_code', mj_plug_class_code)
+		data_merged.set_value(idx, 'base_type_shoulder', base_type_shoulder)
+		data_merged.set_value(idx, 'base_type_flatbottom', base_type_flatbottom)
+		data_merged.set_value(idx, 'base_type_saddle', base_type_saddle)
+		data_merged.set_value(idx, 'hex_size', hex_size)
+		data_merged.set_value(idx, 'seat_angle', seat_angle)
+		data_merged.set_value(idx, 'total_thread_size', thread_size)
+		data_merged.set_value(idx, 'type_boss', type_boss)
+		data_merged.set_value(idx, 'type_stud', type_stud)
+
+		for i in end_form_ids:
+			data_merged.set_value(idx, i, end_form_ids[i])
+
+		data_merged.set_value(idx, 'intended_nut_pitch', intended_nut_pitch)
+		data_merged.set_value(idx, 'bolt_pattern_wide', bolt_pattern_wide)
+		data_merged.set_value(idx, 'plug_diameter', plug_diameter)
+		data_merged.set_value(idx, 'hex_nut_size', hex_nut_size)
+		data_merged.set_value(idx, 'height_over_tube', height_over_tube)
+		data_merged.set_value(idx, 'shoulder_diameter', shoulder_diameter)
+		data_merged.set_value(idx, 'overall_length', overall_length)
+		data_merged.set_value(idx, 'extension_length', extension_length)
+		data_merged.set_value(idx, 'intended_nut_thread', intended_nut_thread)
+		data_merged.set_value(idx, 'mj_class_code_exists', mj_class_code_exists)
+		data_merged.set_value(idx, 'unique_feature_yes', unique_feature_yes)
+		data_merged.set_value(idx, 'unique_feature_no', unique_feature_no)
+		data_merged.set_value(idx, 'drop_length', drop_length)
+		data_merged.set_value(idx, 'elbow_angle', elbow_angle)
+		data_merged.set_value(idx, 'bolt_pattern_long', total_base_diameter)
+
+		for i in thread_pitches:
+			data_merged.set_value(idx, i, thread_pitches[i])
+		data_merged.set_value(idx, 'groove_yes', groove_yes)
+		data_merged.set_value(idx, 'groove_no', groove_no)
+
+	print "Counter"
+	print counter
+
+
+	from sets import Set
+	values = Set([])
+	for entry in comp_variables:
+		if 'unique_feature' in comp_variables[entry]:
+
+			values.add(comp_variables[entry]['unique_feature'])
+	print values
+
+
+
 	return data_merged
 
 def output_final_model(X_train, y_train, X_test, clf, submission_filename):
@@ -92,79 +377,12 @@ if __name__ == '__main__':
 	train = extract(train)
 	test = extract(test)
 
-	#Additional processing of component data and adding it to train/test.
-	comp_files = ['comp_adaptor.csv', 'comp_boss.csv', 'comp_elbow.csv', 'comp_float.csv', 'comp_hfl.csv', 'comp_nut.csv', 'comp_other.csv', 'comp_sleeve.csv', 'comp_straight.csv', 'comp_tee.csv', 'comp_threaded.csv']
-	component_names = ['component_id_' + str(x) for x in range(1, 9)]
-	concat_final_output = []
-	for comp_filename in comp_files:
-	    comp_file = pd.read_csv('competition_data/' + comp_filename)
-	    data_frames_to_concat = []
-	    for name in component_names:
-	        to_merge = train[[name, 'cost']]
-	        data_merged = pd.merge(left = comp_file, right = to_merge, how='left', left_on='component_id', right_on = name)
-	        data_merged = data_merged.drop(name, 1)
-	        data_frames_to_concat.append(data_merged)
-	    final = pd.concat(data_frames_to_concat, ignore_index = True)
-
-	    #Fill in NAs and make sure columns are appropriate data types
-	    for column in final:
-	        if column[:12] == 'component_id':
-	            continue
-	        elif column == 'cost':
-	            continue
-	        elif final[column].dtype == 'object':
-	            final[column] = LabelEncoder().fit_transform(final[column])
-	        elif final[column].dtype == 'float64':
-	            final[column] = final[column].fillna(-1)
-
-	    y_train = final.loc[final['cost'].notnull(),'cost']
-	    y_train = np.log(y_train + 1)
-	    X_train = final.loc[final['cost'].notnull()]
-	    X_train = X_train.drop('cost', 1)
-	    X_test = final.loc[final['cost'].isnull()]
-	    X_test = X_test.drop('cost', 1)
-
-	    clf = RandomForestRegressor(n_estimators = 20)
-
-	    #Want to include all columns except 'component_id'
-	    cols = [x for x in X_test.columns.values if x != 'component_id']
-
-	    clf.fit(X_train[cols], y_train)
-	    predictions = clf.predict(X_test[cols])
-	    predictions = np.exp(predictions) - 1
-	    y_train = np.exp(y_train) - 1
-	    train_output = pd.concat([X_train, y_train], axis=1)
-	    X_test.reindex(index = range(len(X_test)))
-	    X_test['cost'] = predictions
-	    final_output = pd.concat([X_test, train_output])
-	    final_output.to_csv('component_data/' + comp_filename + '_concat_with_predictions.csv', index=False)
-	    concat_final_output.append(final_output.groupby(final_output['component_id']).mean()['cost'])
-
-	output = pd.concat(concat_final_output)
-	output.to_csv('component_data/final.csv')
-
-	comp_dict = {}
-	for idx, row in output.iteritems():
-	    comp_dict[idx] = row
-
-	for idx, row in train.iterrows():
-	    for name in component_names:
-	        if row[name] in comp_dict:
-	            train.set_value(idx, name + '_comp_cost', comp_dict[row[name]])
-	        else:
-	            train.set_value(idx, name + '_comp_cost', 0)
-	            
-	train.to_csv('training_extracted.csv', index=False)
-
-	for idx, row in test.iterrows():
-	    for name in component_names:
-	        if row[name] in comp_dict:
-	            test.set_value(idx, name + '_comp_cost', comp_dict[row[name]])
-	        else:
-	            test.set_value(idx, name + '_comp_cost', 0)
 
 
-	#Gather features to use - don't want to use "cost" nor any feature with an object data type.
+	test.to_csv('extracted_test.csv', index=False)
+	train.to_csv('extracted_train.csv', index=False)
+
+    #Gather features to use - don't want to use "cost" nor any feature with an object data type.
 	feature_names = []
 	for feature in train:
 		if train[feature].dtype != 'object' and feature != 'cost':
